c6dcd3def9fc394b60016df1b3311afa4e14b541
Dhruvi Kothari <dhruvi.kothari03@gmail.com>
2024-03-02 16:46:08 +0530

    connecting to db

---
+++ db_connection.php
@@ -0,0 +1,23 @@
+<?php
+// Database connection parameters
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "manharplot";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+    // echo("Connected successfully");
+}
+
+// Set charset to UTF-8
+$conn->set_charset("utf8");
+
+// You can use this $conn object in other files to perform database operations
+?>
